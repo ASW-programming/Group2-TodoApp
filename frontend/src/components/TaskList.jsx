61f3cc04d8cd1b37c2f3d8418d04ef2d3f8c1825
ASW-programming <adamwinkler7@gmail.com>
2026-03-05 09:48:05 +0100

    Fetch data and display list

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function TaskList() {
-	const numbers = [1, 2, 3, 4];
+	const link = "http://localhost:3000";
+
+	async function getData() {
+		try {
+			const res = await fetch(`${link}/getTodos`);
+
+			if (!res.ok) {
+				throw new Error("Kunde inte hämta todos");
+			}
+
+			const data = await res.json();
+			return data;
+		} catch (error) {
+			console.log("Could not fetch todos");
+		}
+	}
+
+	const {
+		data: todos,
+		isLoading,
+		isError,
+		error,
+	} = useQuery({ queryKey: ["getTodos"], queryFn: getData });
+
+	if (isLoading) {
+		return <p>laddar todos</p>;
+	}
+
+	if (isError) {
+		return <p>ett fel uppstod: {error.message}</p>;
+	}
+
 	return (
 		<div>
 			<ul>
-				{numbers.map((number) => (
-					<li>{numbers}</li>
+				{todos.map((todo) => (
+					<li key={todo.id}>{todo.title}</li>
 				))}
 			</ul>
 		</div>
