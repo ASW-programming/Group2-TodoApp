3debbf8bda487522f4aea4be8c9775e11d41d5d1
Biixie <biixie@outlook.com>
2026-03-05 11:54:12 +0100

    Frontend post method

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -2,11 +2,31 @@ function TodoForm() {
     const [task, setTask] = useState("");
 
     // Förhindrar att sidan laddas om vid submit
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         // Stoppar default behavior
         e.preventDefault();
 
         if (!task.trim()) return;
+
+        // Post 
+
+        try {
+            const postTodo = await fetch("https://localhost.com/addTodo", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ text: task })
+
+            })
+            if (!postTodo.ok) throw new Error("Failed to create todo")
+
+            const data = await postTodo.json();
+
+            console.log("Created todo:", data);
+
+
+        } catch (error) {
+            console.error("Error creating todo:", error);
+        }
     }
 
     return (
