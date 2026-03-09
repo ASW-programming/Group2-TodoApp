c1ef4f3f5377a3b46dd907a7d08a4758ab7e03fe
ASW-programming <adamwinkler7@gmail.com>
2026-03-09 10:39:40 +0100

    Resolve merge conflicts

---
+++ frontend/src/components/TodoForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import TodoInput from "./TodoInput";
+
+function TodoForm() {
+	// Sparar värdet från inputfältet
+	const [task, setTask] = useState("");
+
+	// Förhindrar att sidan laddas om vid submit
+	const handleSubmit = (e) => {
+		// Stoppar default behavior
+		e.preventDefault();
+
+		// Kolla ifall input är tomt
+		if (!task.trim()) return;
+	};
+
+	return (
+		<div>
+			<form id="todoForm" onSubmit={handleSubmit}>
+				{/* Skickar task och setTask till TodoInput via props */}
+				<TodoInput task={task} setTask={setTask} />
+			</form>
+		</div>
+	);
+}
+
+export default TodoForm;
