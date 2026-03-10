084343c425f036830fdacb2bd9e233d5e969d416
Kim Karlsson <kimelliotkarlsson@gmail.com>
2026-03-10 12:32:52 +0100

    Add editTodo function with console log

--- frontend/src/components/EditBtn.jsx
+++ frontend/src/components/EditBtn.jsx
@@ -1,9 +1,14 @@
 function EditBtn(props) {
   
+    const api_url = "http://localhost:3000";
+
+    async function editTodo(id) {
+        console.log('Editing', id);
+    }
+
     return (
         <button>Edit</button>
     );
-
 }
 
 export default EditBtn;
