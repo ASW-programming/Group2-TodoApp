2d035e819c799db3118039a3f0e79e833de4fd8e
Biixie <biixie@outlook.com>
2026-03-16 09:18:32 +0100

    styling and title

--- frontend/src/components/TodoInput.jsx
+++ frontend/src/components/TodoInput.jsx
@@ -1,18 +1,18 @@
 // Tar emot task och setTask från TodoForm via props
 function TodoInput(props) {
-	return (
-		<div className="inputContainer">
-			<input
-				type="text"
-				id="inputField"
-				className="todoInput"
-				value={props.task}
-				onChange={(e) => props.setTask(e.target.value)}
-				placeholder="Enter text"
-			/>
+    return (
+        <div className="inputContainer">
+            <input
+                type="text"
+                id="inputField"
+                className="todoInput"
+                value={props.task}
+                onChange={(e) => props.setTask(e.target.value)}
+                placeholder="I need to..."
+            />
 
-			<span className="inputHighlight"></span>
-		</div>
-	);
+            <span className="inputHighlight"></span>
+        </div>
+    );
 }
 export default TodoInput;
